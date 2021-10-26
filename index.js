3657b842a239c484823b6fd32a7347c5f5284aba
rashidulkarim <rashidul.karim7@gmail.com>
2021-10-26 21:34:40 +0600

    u

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run()=>{
+async function run(){
     try{
         await client.connect();
         const database = client.db(`${process.env.DB_NAME}`)
